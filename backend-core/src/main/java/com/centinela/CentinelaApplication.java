c4219ffd2b60a1b0403e4e6f7786cc5863a4d7a8
Brayan Chavez <brayanjairchavez981@gmail.com>
2026-06-15 20:41:10 -0500

    feat(core): inicializacion de infraestructura docker y backend spring boot

---
+++ backend-core/src/main/java/com/centinela/CentinelaApplication.java
@@ -0,0 +1,13 @@
+package com.centinela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentinelaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentinelaApplication.class, args);
+    }
+
+}
